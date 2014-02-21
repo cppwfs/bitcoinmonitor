fdca466690a889d48b28f4a602619d9664354079
Glenn Renfro <grenfro@gopivotal.com>
2014-02-21 16:03:24 -0500

    Added SI components

--- src/main/java/org/bitcoinmonitor/Main.java
+++ src/main/java/org/bitcoinmonitor/Main.java
@@ -1,10 +1,11 @@
 package org.bitcoinmonitor;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-
+@ComponentScan
 @Configuration
 @ImportResource("integration.xml")
 public class Main {
--- src/main/java/org/bitcoinmonitor/RawData.java
+++ src/main/java/org/bitcoinmonitor/RawData.java
@@ -1,5 +1,30 @@
 package org.bitcoinmonitor;
 
+
 public class RawData {
+	String bid;
+	String last;
+
+	public String getBid() {
+		return bid;
+	}
+
+	public void setBid(String bid) {
+		this.bid = bid;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+
+	@Override
+	public String toString() {
+		return "RawData [bid=" + bid + ", last=" + last + "]";
+	}
+
 
 }
