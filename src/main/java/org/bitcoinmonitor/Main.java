49fbe60959bbe0e05a6fb5cf7420cee54dd4c5d2
Glenn Renfro <grenfro@gopivotal.com>
2014-02-22 14:16:35 -0500

    Updated to setup labs

--- src/main/java/org/bitcoinmonitor/Main.java
+++ src/main/java/org/bitcoinmonitor/Main.java
@@ -5,13 +5,17 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-@ComponentScan
+// Phase 3
+//@ComponentScan
+// Phase 2
 @Configuration
+// Phase 2
 @ImportResource("integration.xml")
 public class Main {
 
 		    public static void main(String[] args) {
 		    	System.out.println("Hello world");
+//Phase 2
 		        new AnnotationConfigApplicationContext(Main.class);
 		    }
 }
